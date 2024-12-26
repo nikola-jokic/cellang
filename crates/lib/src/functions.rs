65eba6b3e34ec6376453a4d3f897470c2ca560e3
Nikola Jokic <jokicnikola07@gmail.com>
2024-12-26 15:02:42 +0100

    Implement map and filter

--- crates/lib/src/functions.rs
+++ crates/lib/src/functions.rs
@@ -1,33 +1,35 @@
 use crate::{
     eval_ast,
     parser::{Atom, Op, TokenTree},
-    types::Map,
+    types::{List, Map},
     Environment, Key, KeyType, Value,
 };
 use miette::Error;
 
-pub fn size(env: &Environment, vals: &[TokenTree]) -> Result<Value, Error> {
-    if vals.len() != 1 {
-        miette::bail!("Expected 1 argument, found {}", vals.len());
+/// Returns the size of a value.
+/// This is an implementation of https://github.com/google/cel-spec/blob/master/doc/langdef.md#functions
+pub fn size(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Error> {
+    if tokens.len() != 1 {
+        miette::bail!("Expected 1 argument, found {}", tokens.len());
     }
 
-    let v = match eval_ast(env, &vals[0])?.to_value(env)? {
+    let v = match eval_ast(env, &tokens[0])?.to_value(env)? {
         Value::Bytes(b) => Value::Int(b.len() as i64),
         Value::String(s) => Value::Int(s.len() as i64),
         Value::List(list) => Value::Int(list.len() as i64),
         Value::Map(map) => Value::Int(map.len() as i64),
-        _ => miette::bail!("Invalid type for size: {:?}", vals[0]),
+        _ => miette::bail!("Invalid type for size: {:?}", tokens[0]),
     };
 
     Ok(v)
 }
 
-pub fn type_fn(env: &Environment, vals: &[TokenTree]) -> Result<Value, Error> {
-    if vals.len() != 1 {
-        miette::bail!("Expected 1 argument, found {}", vals.len());
+pub fn type_fn(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Error> {
+    if tokens.len() != 1 {
+        miette::bail!("Expected 1 argument, found {}", tokens.len());
     }
 
-    let v = match eval_ast(env, &vals[0])?.to_value(env)?.downcast() {
+    let v = match eval_ast(env, &tokens[0])?.to_value(env)?.downcast() {
         Value::Int(_) => Value::String("int".to_string()),
         Value::Uint(_) => Value::String("uint".to_string()),
         Value::Double(_) => Value::String("double".to_string()),
@@ -43,22 +45,22 @@ pub fn type_fn(env: &Environment, vals: &[TokenTree]) -> Result<Value, Error> {
     Ok(v)
 }
 
-pub fn has(env: &Environment, vals: &[TokenTree]) -> Result<Value, Error> {
-    if vals.len() != 1 {
-        miette::bail!("expected 1 argument, found {}", vals.len());
+pub fn has(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Error> {
+    if tokens.len() != 1 {
+        miette::bail!("expected 1 argument, found {}", tokens.len());
     }
 
-    match &vals[0] {
+    match &tokens[0] {
         TokenTree::Cons(op, tokens) if matches!(op, Op::Field | Op::Index) => {
             let map = match eval_ast(env, &tokens[0])?.to_value(env)? {
                 Value::Map(m) => m,
-                _ => miette::bail!("Invalid type for has: {:?}", vals[0]),
+                _ => miette::bail!("Invalid type for has: {:?}", tokens[0]),
             };
 
             match &tokens[1] {
                 TokenTree::Atom(Atom::Ident(ident)) => {
                     if !matches!(*op, Op::Field) {
-                        miette::bail!("Invalid type for has: {:?}", vals[0]);
+                        miette::bail!("Invalid type for has: {:?}", tokens[0]);
                     }
                     let key = Key::String(ident.to_string());
                     Ok(Value::Bool(map.contains_key(&key)?))
@@ -203,10 +205,143 @@ pub fn exists_one(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Erro
     Ok(Value::Bool(found))
 }
 
+pub fn map(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Error> {
+    if tokens.len() != 3 || tokens.len() != 4 {
+        miette::bail!("expected 2 or 3 arguments, found {}", tokens.len());
+    }
+
+    let this = if tokens.len() == 3 {
+        match eval_ast(env, &tokens[0])?.to_value(env)? {
+            v if matches!(v, Value::List(_) | Value::Map(_)) => v,
+            _ => miette::bail!("Invalid type for map: {:?}", tokens[0]),
+        }
+    } else {
+        filter(env, &tokens[0..3])?
+    };
+
+    let key = match &tokens[1] {
+        TokenTree::Atom(Atom::Ident(ident)) => Key::String(ident.to_string()),
+        _ => miette::bail!("Invalid type for map: {:?}", tokens[1]),
+    };
+
+    let lambda = if tokens.len() == 3 {
+        &tokens[2]
+    } else {
+        &tokens[3]
+    };
+
+    match this {
+        Value::List(list) => {
+            if list.is_empty() {
+                return Ok(Value::List(list));
+            }
+            let mut env = env.new_child();
+            let mut new_list =
+                List::with_type_and_capacity(list.element_type().unwrap(), list.len());
+
+            for item in list.iter() {
+                env.variables = Map::with_type_and_capacity(KeyType::String, 1);
+                env.variables.insert(key.clone(), item.clone())?;
+                new_list.push(eval_ast(&env, lambda)?.to_value(&env)?)?;
+            }
+            Ok(Value::List(new_list))
+        }
+        Value::Map(map) => {
+            if map.is_empty() {
+                return Ok(Value::Map(map));
+            }
+            let mut env = env.new_child();
+            let mut new_map = Map::with_capacity(map.len());
+            for (key, value) in map.iter() {
+                env.variables = Map::with_type_and_capacity(KeyType::String, 1);
+                env.variables
+                    .insert(Key::String("this".to_string()), value.clone())?;
+                new_map.insert(key.clone(), eval_ast(&env, lambda)?.to_value(&env)?)?;
+            }
+            Ok(Value::Map(new_map))
+        }
+        _ => unreachable!(),
+    }
+}
+
+pub fn filter(env: &Environment, tokens: &[TokenTree]) -> Result<Value, Error> {
+    if tokens.len() != 2 {
+        miette::bail!("expected 2 arguments, found {}", tokens.len());
+    }
+    let host = match eval_ast(env, &tokens[0])?.to_value(env)? {
+        v if matches!(v, Value::List(_) | Value::Map(_)) => v,
+        _ => miette::bail!("Invalid type for filter: {:?}", tokens[0]),
+    };
+
+    // expect second parameter to be an identifier
+    let ident = match &tokens[1] {
+        TokenTree::Atom(Atom::Ident(ident)) => ident,
+        _ => miette::bail!("Invalid type for exists_one: {:?}", tokens[1]),
+    };
+
+    // expect third parameter to be a lambda
+    let lambda = &tokens[2];
+
+    let mut env = env.new_child();
+    let key = Key::String(ident.to_string());
+    let mut variables = Map::with_type_and_capacity(KeyType::String, 1);
+
+    match host {
+        Value::List(list) => {
+            let mut new_list =
+                List::with_type_and_capacity(list.element_type().unwrap(), list.len());
+            for item in list.iter() {
+                variables.insert(key.clone(), item.clone())?;
+                env.variables = variables.clone();
+                match eval_ast(&env, lambda)?.to_value(&env)? {
+                    Value::Bool(b) => {
+                        if b {
+                            new_list.push(item.clone())?;
+                        }
+                    }
+                    _ => miette::bail!("Invalid type for filter: {:?}", lambda),
+                }
+            }
+            Ok(Value::List(new_list))
+        }
+        Value::Map(map) => {
+            let mut new_map = Map::with_capacity(map.len());
+            for (key, value) in map.iter() {
+                variables.insert(key.clone(), value.clone())?;
+                env.variables = variables.clone();
+                match eval_ast(&env, lambda)?.to_value(&env)? {
+                    Value::Bool(b) => {
+                        if b {
+                            new_map.insert(key.clone(), value.clone())?;
+                        }
+                    }
+                    _ => miette::bail!("Invalid type for filter: {:?}", lambda),
+                }
+            }
+            Ok(Value::Map(new_map))
+        }
+        _ => unreachable!(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Parser;
+    use crate::{Function, Parser};
+
+    fn is_function(_: Function) {}
+
+    #[test]
+    fn implement_functions() {
+        is_function(Box::new(size));
+        is_function(Box::new(type_fn));
+        is_function(Box::new(has));
+        is_function(Box::new(all));
+        is_function(Box::new(exists));
+        is_function(Box::new(exists_one));
+        is_function(Box::new(map));
+        is_function(Box::new(filter));
+    }
 
     #[test]
     fn test_size_primitive() {
--- crates/lib/src/types.rs
+++ crates/lib/src/types.rs
@@ -1,8 +1,9 @@
 use miette::Error;
 use serde::{ser::Serializer, Deserialize, Serialize};
-use std::collections::hash_map::{Drain, Entry, IntoValues, Iter, IterMut, Keys, RandomState};
+use std::collections::hash_map::{self, Entry, IntoValues, Iter, IterMut, Keys, RandomState};
 use std::collections::HashMap;
 use std::fmt;
+use std::vec;
 
 use crate::parser::TokenTree;
 use crate::{impl_value_conversions, Environment};
@@ -79,7 +80,7 @@ impl Map {
 
     /// Wrapper for drain
     /// https://doc.rust-lang.org/std/collections/struct.HashMap.html#method.drain
-    pub fn drain(&mut self) -> Drain<'_, Key, Value> {
+    pub fn drain(&mut self) -> hash_map::Drain<'_, Key, Value> {
         self.inner.drain()
     }
 
@@ -586,11 +587,19 @@ impl List {
         }
     }
 
+    pub fn into_iter(self) -> vec::IntoIter<Value> {
+        self.inner.into_iter()
+    }
+
+    pub fn element_type(&self) -> Option<ValueKind> {
+        self.elem_type.clone()
+    }
+
     pub fn iter(&self) -> impl Iterator<Item = &Value> {
         self.inner.iter()
     }
 
-    pub fn new_with_type(elem_type: ValueKind) -> Self {
+    pub fn with_type(elem_type: ValueKind) -> Self {
         Self {
             elem_type: Some(elem_type),
             inner: Vec::new(),
@@ -620,6 +629,218 @@ impl List {
                 Ok(self.inner.contains(value))
             }
         }
+    }
+
+    pub fn append(&mut self, other: &mut List) -> Result<&mut Self, Error> {
+        if let Some(ref elem_type) = self.elem_type {
+            if let Some(ref other_elem_type) = other.elem_type {
+                if *elem_type != *other_elem_type {
+                    miette::bail!(
+                        "Invalid element type: {:?} and {:?}",
+                        elem_type,
+                        other_elem_type
+                    );
+                }
+            }
+        } else {
+            self.elem_type = other.elem_type.take();
+        }
+
+        self.inner.append(&mut other.inner);
+        Ok(self)
+    }
+
+    pub fn push(&mut self, value: Value) -> Result<&mut Self, Error> {
+        self.prepare_insert(&value)?;
+        self.inner.push(value);
+        Ok(self)
+    }
+
+    pub fn drain<R>(&mut self, range: R) -> vec::Drain<'_, Value>
+    where
+        R: std::ops::RangeBounds<usize>,
+    {
+        self.inner.drain(range)
+    }
+
+    pub fn as_mut_ptr(&mut self) -> *mut Value {
+        self.inner.as_mut_ptr()
+    }
+
+    pub fn as_mut_slice(&mut self) -> &mut [Value] {
+        self.inner.as_mut_slice()
+    }
+
+    pub fn as_ptr(&self) -> *const Value {
+        self.inner.as_ptr()
+    }
+
+    pub fn as_slice(&self) -> &[Value] {
+        self.inner.as_slice()
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.inner.capacity()
+    }
+
+    pub fn clear(&mut self) {
+        self.inner.clear();
+    }
+
+    pub fn dedup(&mut self) {
+        self.inner.dedup()
+    }
+
+    pub fn dedup_by<F>(&mut self, same_bucket: F)
+    where
+        F: FnMut(&mut Value, &mut Value) -> bool,
+    {
+        self.inner.dedup_by(same_bucket)
+    }
+
+    pub fn dedup_by_key<F, K>(&mut self, key: F)
+    where
+        F: FnMut(&mut Value) -> K,
+        K: PartialEq<K>,
+    {
+        self.inner.dedup_by_key(key)
+    }
+
+    pub fn extend_from_slice(&mut self, other: &[Value]) -> Result<(), Error> {
+        if other.is_empty() {
+            return Ok(());
+        }
+
+        other.iter().try_for_each(|v| self.prepare_insert(v))?;
+        self.inner.extend_from_slice(other);
+        Ok(())
+    }
+
+    pub fn extend_from_within<R>(&mut self, src: R)
+    where
+        R: std::ops::RangeBounds<usize>,
+    {
+        self.inner.extend_from_within(src);
+    }
+
+    pub fn insert(&mut self, index: usize, element: Value) {
+        self.prepare_insert(&element).unwrap();
+        self.inner.insert(index, element)
+    }
+
+    pub fn into_boxed_slice(self) -> Box<[Value]> {
+        self.inner.into_boxed_slice()
+    }
+
+    pub fn leak<'a>(self) -> &'a [Value] {
+        self.inner.leak()
+    }
+
+    pub fn pop(&mut self) -> Option<Value> {
+        self.inner.pop()
+    }
+
+    pub fn remove(&mut self, index: usize) -> Value {
+        self.inner.remove(index)
+    }
+
+    pub fn reserve(&mut self, additional: usize) {
+        self.inner.reserve(additional)
+    }
+
+    pub fn reserve_exact(&mut self, additional: usize) {
+        self.inner.reserve_exact(additional)
+    }
+
+    pub fn resize(&mut self, new_len: usize, value: Value) {
+        self.prepare_insert(&value).unwrap();
+        self.inner.resize(new_len, value)
+    }
+
+    pub fn resize_with<F>(&mut self, new_len: usize, f: F)
+    where
+        F: FnMut() -> Value,
+    {
+        self.inner.resize_with(new_len, f)
+    }
+
+    pub fn retain<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&Value) -> bool,
+    {
+        self.inner.retain(|v| f(v))
+    }
+
+    pub fn retain_mut<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut Value) -> bool,
+    {
+        self.inner.retain_mut(|v| f(v))
+    }
+
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        self.inner.shrink_to(min_capacity)
+    }
+
+    pub fn shrink_to_fit(&mut self) {
+        self.inner.shrink_to_fit()
+    }
+
+    pub fn split_off(&mut self, at: usize) -> List {
+        List {
+            elem_type: self.elem_type.clone(),
+            inner: self.inner.split_off(at),
+        }
+    }
+
+    pub fn swap_remove(&mut self, index: usize) -> Value {
+        self.inner.swap_remove(index)
+    }
+
+    pub fn truncate(&mut self, len: usize) {
+        self.inner.truncate(len)
+    }
+
+    pub fn try_reserve(&mut self, additional: usize) -> Result<(), Error> {
+        match self.inner.try_reserve(additional) {
+            Ok(_) => Ok(()),
+            Err(e) => miette::bail!(e),
+        }
+    }
+
+    pub fn try_reserve_exact(&mut self, additional: usize) -> Result<(), Error> {
+        match self.inner.try_reserve_exact(additional) {
+            Ok(_) => Ok(()),
+            Err(e) => miette::bail!(e),
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elem_type: None,
+            inner: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn with_type_and_capacity(elem_type: ValueKind, capacity: usize) -> Self {
+        Self {
+            elem_type: Some(elem_type),
+            inner: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn prepare_insert(&mut self, other: &Value) -> Result<(), Error> {
+        match self.elem_type {
+            None => {
+                self.elem_type = Some(other.kind());
+            }
+            Some(ref elem_type) => {
+                if *elem_type != other.kind() {
+                    miette::bail!("Invalid element type: {:?}", other.kind());
+                }
+            }
+        }
+        Ok(())
     }
 }
 
